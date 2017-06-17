f4cea0174709ccace8ca33b1ccd20638f92cd428
Maro <grafika.marekduda@gmail.com>
2017-06-18 00:39:50 +0200

    deleted empty lines

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,17 @@
 $(function() {
-
     var $carouselList = $('#carousel ul'),
-        $interval;
-		
-
+        interval;
     function changeSlide() {
         $carouselList.animate({'margin-left': -700}, 500, moveFirstSlide);
     };
-
     function moveFirstSlide() {
         $firstItem = $carouselList.find('li:first'),
         $lastItem = $carouselList.find('li:last');
         $lastItem.after($firstItem);
         $carouselList.css({'margin-left': 0});
     };
-
     function startSlide() {
-        $interval = setInterval(changeSlide, 2500);
+        interval = setInterval(changeSlide, 2500);
     };
-
-    startSlide();
-	
+    startSlide();	
 });
